5e875d7af66f581b993bddac1b9bea552d43ffee
刘文文 <tcliuwenwen@126.com>
2019-01-24 17:00:19 +0800

    Third Homework

--- src/main/java/com/youkeda/wacai/web/control/AccountingController.java
+++ src/main/java/com/youkeda/wacai/web/control/AccountingController.java
@@ -1,15 +1,15 @@
 package com.youkeda.wacai.web.control;
 
+import com.youkeda.wacai.web.model.Accounting;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountingController {
     @RequestMapping(path = "/accounting")
-    public String add(@RequestParam("cash") int key1, @RequestParam("income") int key2, @RequestParam("rent") int key3, @RequestParam("charges") int key4, @RequestParam("eat") int key5) {
-        int result = key1 + key2 - key3 - key4 - key5;
-        String s = String.valueOf(result);
-        return "本月余额为:" + s;
+    public Accounting account(Accounting accounting) {
+        int result = accounting.getCash()+accounting.getIncome()-accounting.getRent()-accounting.getCharges()-accounting.getEat()-accounting.getTreat()-accounting.getKtv();
+        accounting.setBalance(result);
+        return accounting;
     }
 }
