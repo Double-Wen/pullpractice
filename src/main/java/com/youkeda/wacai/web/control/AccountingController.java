6e2b89df202284d5cf09faf0866ee244c5c30ecd
刘文文 <tcliuwenwen@126.com>
2019-02-14 13:11:49 +0800

    Update AccountingController.java

--- src/main/java/com/youkeda/wacai/web/control/AccountingController.java
+++ src/main/java/com/youkeda/wacai/web/control/AccountingController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AccountingController {
     @RequestMapping(path = "/accounting")
-    public String add(@RequestParam("cash") int key1, @RequestParam("income") int key2, @RequestParam("rent") int key3, @RequestParam("charges") int key4, @RequestParam("eat") int key5) {
+    public String accounting(@RequestParam("cash") int key1, @RequestParam("income") int key2, @RequestParam("rent") int key3, @RequestParam("charges") int key4, @RequestParam("eat") int key5) {
         int result = key1 + key2 - key3 - key4 - key5;
         String s = String.valueOf(result);
         return "本月余额为:" + s;
